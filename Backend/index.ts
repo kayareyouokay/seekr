3b808544e2ef77f3b0b0b6edd8687f2aa6813968
Kay <krishbhardwaj0619@gmail.com>
2026-04-26 22:11:14 +0530

    chore: bootstrap seekr API with web search and LLM response streaming

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,1 +1,52 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { tavily } from "@tavily/core";
+import { GoogleGenAI } from "@google/genai";
+import { PROMPT_TEMPLATE, SYSTEM_PROMPT } from "./prompt";
+
+const app = express();
+const ai = new GoogleGenAI({});
+const client = tavily({ apiKey: process.env.TAVILY_API_KEY });
+const PORT = 3000;
+
+app.use(express.json());
+
+app.post('/ask', async (req, res) => {
+    // STEP 1 : Get the query from user
+    const { query } = req.body;
+
+    // STEP 2 : Make sure the user has access/credits to hit the end point
+
+    // STEP 3 : Check if we have web search indexed for a similar query
+
+    // STEP 4 : Web search to gather resources
+    const webSearchResponse  = await client.search(query, {
+        searchDepth: "advanced",
+    })
+
+    const webSearchResults = webSearchResponse.results;
+
+    // STEP 5: Do some context engineering on the prompt + web search responses
+
+    // STEP 6 : Hit the LLM and stream back the response 
+    const prompt = PROMPT_TEMPLATE
+        .replace('{{USER_QUERY}}', query)
+        .replace('{{WEB_SEARCH_RESULTS}}', JSON.stringify(webSearchResults));
+    
+    const response = await ai.models.generateContentStream({
+        model: "gemini-3-flash-preview",
+        contents: prompt,
+        config: {
+            systemInstruction: SYSTEM_PROMPT
+        }
+    });
+
+    for await (const chunk of response) {
+        console.log(chunk.text);
+    }
+
+    // STEP 7 : Also stream back the ources and follow up questions (hich we can get from another parallel LLM call )
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started listening on Port:${PORT}`);
+})
